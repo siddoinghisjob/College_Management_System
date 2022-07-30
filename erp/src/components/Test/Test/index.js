19d04ad468532f452ef5c2709c9a461884fea4bf
siddoinghisjob <btech10685.21@bitmesra.ac.in>
2022-07-30 20:53:23 +0530

    remove zustand dependency

--- erp/src/components/Test/Test/index.js
+++ erp/src/components/Test/Test/index.js
@@ -1,20 +1,28 @@
-import {PlusIcon} from '@heroicons/react/solid'
-import React, {useState} from 'react'
+import React, {useState} from "react"
+import {PlusIcon, XCircleIcon} from "@heroicons/react/solid"
 
-function AnswerBox()
+function AnswerBox(props)
 {
-    const [state,setState] = React.useState({
-        popUp: true
-    })
-
+    const [state,setState] = React.useState(false)
+    const [number,setNumber] = React.useState(1)
     return(
-        <div className='relative'>
-            
-            <form className='z-[-1]'>
+        <div className="">
+            <div className={state ? "absolute flex flex-col z-1 bg-gradient-to-r from-gray-900 to-gray-700 text-white rounded-xl shadow opacity-[0.97] p-5 text-3xl h-auto w-full" : "absolute scale-0 opacity-0 h-0 w-0"}>
+                <div className="flex flex-row">
+                    <XCircleIcon className="left-0 top-0 absolute h-10 w-10 cursor-pointer" onClick={() => setState(false)}/>
+                    &nbsp;&nbsp;<h1 className="m-auto mb-5"> <span className="text-3xl px-2 bg-gradient-to-br from-indigo-700 to-blue-600 rounded ">AnswerBox</span> for Question {props.num}&nbsp;&nbsp;</h1>
+                    <PlusIcon className="right-[0.3rem] absolute top-[0.2rem] h-8 w-8 cursor-pointer bg-white text-black rounded-full"/>
+                </div>                        
+                <div className="flex items-center align-middle">
+                    {
+                        [...Array(number)].map((e,i) =><span className="p-3 flex items-center align-middle"><input type="checkbox" className="h-8 w-8 accent-green-500 bg-green-800"/><span></span><input className="h-8 w-[80%]"/></span>)
+                    }
+                </div>
+            </div>
+            <form className=''>
                 <div className="flex flex-row items-center">
-                    <h1 className="p-2 text-xl border-r-0 h-12 border-2 border-black">Q1</h1>
-                    <textarea className="border-2 border-black h-12 w-96"/>
-                    <PlusIcon className='h-12 w-auto bg-green-400 shadow-2xl hover:bg-green-700 rounded-full cursor-pointer ml-1 text-white'/>
+                    <h1 className="p-2 text-xl border-r-0 h-12 border-2 border-black">Q{props.num}</h1>
+                    <textarea onFocus={() => setState(true)} className="border-2 border-black h-12 w-96"/>
                 </div>
             </form>
         </div>
--- erp/src/components/test.js
+++ erp/src/components/test.js
@@ -1,30 +1,19 @@
-import React, { useState } from "react";
-import { ArrowCircleLeftIcon, StarIcon } from "@heroicons/react/solid";
+import React, { useState, useEffect } from "react";
+import { StarIcon, XCircleIcon, PlusIcon} from "@heroicons/react/solid";
+import {ArrowLeftIcon} from '@heroicons/react/outline'
 import AnswerBox from "./Test/Test";
 
 function Test() {
   const [state, setState] = React.useState({
     show: true,
     title: "",
+    hide:false,
   });
 
+  const [number,setNumber] = React.useState(1)
+
   return (
-    <div className="bg-white relative p-10 rounded flex flex-row items-center align-middle shadow-2xl">
-      <div className="absolute text-4xl top-0 z-50 opacity-95 bg-blue-600 text-white rounded-lg shadow p-5 w-[150%]">
-        <div className="flex flex-col">
-          <div className="flex flex-row items-center m-auto">
-            <StarIcon className="h-[5rem] w-[5rem] rounded-full mr-2 bg-blue-400 text-white rotate-[-12deg] shadow-2xl" />
-            <h1 className="text-[4rem]">Tips</h1>
-          </div>
-          <div>
-            <StarIcon className="h-10 w-10" />
-            How to put a Text Answer?
-          </div>
-          <div className="bg-blue-400 rounded">
-            Write the answer on the first option box and tick it.
-          </div>
-        </div>
-      </div>
+    <div className="bg-white relative p-10 flex flex-row items-center align-middle shadow-2xl rounded-2xl">
       <div
         className={
           state.show
@@ -48,15 +37,45 @@ function Test() {
         className={
           state.show
             ? "absolute scale-0 opacity-0 h-0 w-0"
-            : "flex flex-col items-center align-middle p-3"
+            : "flex flex-col items-center align-middle p-3 relative"
         }
       >
-        <ArrowCircleLeftIcon
+        <div className={state.hide ? "absolute scale-0 h-0 w-0 opacity-0":"absolute transition-all duration-150 origin-bottom text-4xl left-[0rem] h-[110%] top-[-1rem] z-50 opacity-[.93] bg-blue-600 text-white rounded-lg shadow p-8"}>
+          <div className="flex flex-col">
+            <div className="flex flex-row items-center m-auto">
+              <StarIcon className="h-[5rem] w-[5rem] rounded-full mr-2 bg-blue-400 text-yellow-400 rotate-[-12deg] shadow-2xl shadow-blue-800" />
+              <h1 className="text-[4rem]">Tips</h1>
+              <XCircleIcon onClick={() => setState({...state,hide:true})} className="h-[4rem] w-[4rem] rounded-full text-red-400 hover:scale-110 hover:text-red-500 transition-all duration-200 origin-center cursor-pointer absolute right-0 top-0"/>
+            </div>
+            <div className="flex flex-row mb-6 mt-6">
+              <StarIcon className="h-12 w-12 rounded-full bg-blue-400 mr-2" />
+              How to put a Text Answer?
+            </div>
+            <div className="bg-blue-400 rounded p-1 px-2">
+              Write the answer on the first option box and tick it.
+            </div>
+          </div>
+        </div>
+        <ArrowLeftIcon
           onClick={() => setState({ show: true, title: "" })}
-          className="h-16 w-16 mb-4 top-[-1rem] hover:text-red-400 hover:bg-white rounded-full bg-red-400 shadow-2xl text-white cursor-pointer absolute left-[-1rem]"
+          className="h-12 w-12 mb-4 p-1 hover:bg-black top-0 bg-gray-800 text-white rounded-2xl shadow cursor-pointer absolute left-0"
         />
+        <PlusIcon onClick={() => setNumber(number+1)} className="absolute h-12 w-12 top-0 hover:bg-black right-0 bg-gray-800 text-white rounded-2xl shadow cursor-pointer"/>
+        <XCircleIcon onClick={() => setNumber(number-1)} className={number !== 1 ? "h-12 w-12 absolute cursor-pointer bottom-0 right-0 text-red-600 hover:text-red-800" : "absolute scale-0 w-0 h-0 opacity-0"} />
+
         <h1 className="heading mt-3">{state.title}</h1>
-        <AnswerBox />
+        <div className="overflow-y-scroll h-48 scroll">
+          {
+            [...Array(number)].map((e,i) => {
+              return (
+              <div className="flex flex-row items-center">
+                <AnswerBox className="border-collapse" num ={i+1}/>
+              </div>
+              )
+            })
+          }
+        </div>
+        <button className="bg-indigo-600 text-white rounded-md shadow p-3 text-2xl mt-4 font-mono">Submit</button>
       </div>
     </div>
   );
